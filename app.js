d3e64a6e70eb1041fe795626bf1f1e88b4146726
Tom Phillips <eflips@yahoo.com>
2021-06-05 16:55:02 -0700

    page template, and debug app.js (faulty routing)

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const base = require("airtable").base("appsG4CYwtZ7gNOik")
 const express = require("express")
 const path = require("path")
+
 const app = express()
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "."))
-    app.set
-  ; (async () => {
-    const records = await base("Business Hours").select({ view: "Grid view", }).firstPage()
-    for (const record of records) {
-     console.log(record.get("Day"), record.get("Hours"));
-    }
+
+//Now pull the data down from Airtable
+app.get("/", (req, res) => {
+ (async () => {
+    const records = await base("Business Hours")
+      .select({ view: "Grid view" })
+      .firstPage()
+    res.render("page", {
+      records,
+    })
   })()
+})
+app.listen(3000, ()=> console.log("Server is in good shape Captain!"))
